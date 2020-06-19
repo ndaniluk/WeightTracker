8cd7119eae3193a8a757c7ee821caa466b3b93fc
Norbert Daniluk <norbert.daniluk@gmail.com>
2020-06-19 20:07:33 +0200

    Added validator

--- WeightTracker/Entities/MeasurementRecord.cs
+++ WeightTracker/Entities/MeasurementRecord.cs
@@ -1,19 +1,28 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using WeightTracker.Validators;
 
 namespace WeightTracker.Entities
 {
    public class MeasurementRecord
    {
       public int Id { get; set; }
+      [IsNumberGreaterThanZero]
       public float Chest { get; set; }
+      [IsNumberGreaterThanZero]
       public float Biceps { get; set; }
+      [IsNumberGreaterThanZero]
       public float Waist { get; set; }
+      [IsNumberGreaterThanZero]
       public float Hips { get; set; }
+      [IsNumberGreaterThanZero]
       public float Thigh { get; set; }
+      [IsNumberGreaterThanZero]
       public float Calf { get; set; }
+      [IsNumberGreaterThanZero]
       public float Height { get; set; }
+      [IsNumberGreaterThanZero]
       public float Weight { get; set; }
       public DateTime TimeStamp { get; set; }
       public User User { get; set; }
---
+++ WeightTracker/Validators/IsNumberGreaterThanZero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeightTracker.Validators
+{
+   public class IsNumberGreaterThanZero: ValidationAttribute
+   {
+      public override bool IsValid(object value)
+      {
+         return value != null && int.TryParse(value.ToString(), out var i) && i > 0;
+      }
+   }
+}
