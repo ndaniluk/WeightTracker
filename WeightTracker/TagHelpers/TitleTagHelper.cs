ec0b3102467285b25a722af63e3d8a0cc057d38b
Norbert Daniluk <norbert.daniluk@gmail.com>
2020-06-20 21:25:30 +0200

    Added helper and partial

---
+++ WeightTracker/TagHelpers/TitleTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WeightTracker.TagHelpers
+{
+   // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+   [HtmlTargetElement("HelperTitle")]
+   public class TitleTagHelper : TagHelper
+   {
+      public override void Process(TagHelperContext context, TagHelperOutput output)
+      {
+         output.TagName = "h3";
+         output.PreContent.SetHtmlContent("<u>");
+         output.PostContent.SetHtmlContent("</u>");
+      }
+   }
+}
